5af4f0b189f8b15f359649a14c5c8a75b3f75850
Matioma <Artiom.Matievschi@gmail.com>
2020-09-22 16:42:44 +0200

    Render A square

--- Scripts/script.js
+++ Scripts/script.js
@@ -167,8 +167,8 @@ function main(){
     drawScene(gl,programInfo, buffers);
 
 
-    gl.clearColor(0.0,0.0,0.0,1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // gl.clearColor(0.0,0.0,0.0,1.0);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 window.onload = main;
